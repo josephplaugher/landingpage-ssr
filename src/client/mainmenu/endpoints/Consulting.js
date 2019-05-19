2dc5dc21accb936783cf1168f164f234bdfeb9e2
Joseph <joseph@appreciateco.com>
2019-05-19 13:06:05 -0700

    updating reactform-appco

--- src/client/mainmenu/endpoints/Consulting.js
+++ src/client/mainmenu/endpoints/Consulting.js
@@ -5,11 +5,11 @@ const Consulting = () => (
         <h1 className="headline">Consulting</h1>
         <p className="text">
             Maybe you're not sure how or if software can benefit you and your
-            business. We seek to understand your pain point - the part of your
-            business process that is ineffficient, expensive, or just a hassle -
-            and we will work to alleviate the "pain" and automate or streamline
-            it using software. If you feel your business already runs pretty
-            smoothly, we may still be able to improve upon your existing
+            business. We will seek to understand your pain point - the part of
+            your business process that is inefficient, expensive, or just a
+            hassle - and we will work to alleviate the "pain" and automate or
+            streamline it using software. If you feel your business already runs
+            pretty smoothly, we may still be able to improve upon your existing
             systems.
         </p>
         <p className="text">
