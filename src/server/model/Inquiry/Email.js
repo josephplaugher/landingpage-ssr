17ddb105114b3e2ce24b9dde051f6359921fc7c5
Joseph <joseph@appreciateco.com>
2019-05-20 05:45:08 -0700

    updated reactform-appco to not console log in production

--- src/server/model/Inquiry/Email.js
+++ src/server/model/Inquiry/Email.js
@@ -17,8 +17,8 @@ const Email = formData => {
     Message: ${formData.message} `
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"Appreciate Corporation" <joseph@appreciateco.com>', // sender address
-        to: `"${formData.fname}" <${formData.email}>`, // recipient
+        from: '"Appreciate Landing Page" <joseph@appreciateco.com>', // sender address
+        to: '"Appreciate Corporation" <joseph@appreciateco.com>', // recipient
         subject: `New email from landing page:`, // Subject line
         text: body, // plain text body
         html: '<p>' + body + '</p>', // html body
