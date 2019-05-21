f04b87adda89534a4c827465e59154ff3af5e9c2
Joseph <joseph@appreciateco.com>
2019-05-20 17:28:29 -0700

    fixing the email issue

--- src/server.js
+++ src/server.js
@@ -28,10 +28,7 @@ export default function() {
 
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', SetUrl())
-        res.header(
-            'Access-Control-Allow-Methods',
-            'GET,PUT,POST,DELETE,OPTIONS'
-        )
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS')
         res.header(
             'Access-Control-Allow-Headers',
             'Content-Type, authorization'
